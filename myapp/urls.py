2c661da01df87e5eddcc44ab0f01afb2393fb245
akhil chowdary <akhilchowdary.m007@gmail.com>
2020-08-10 18:35:36 +0530

    get post commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,6 @@ app_name="myapp"
 urlpatterns = [
     path('trail/',views.trail,name='trail'),
     path('profile/',views.profile,name='profile'),
-    
+    path('get_demo/',views.get_demo,name="get_demo"),
+    path('post_demo/',views.post_demo,name="post_demo"),
 ]
